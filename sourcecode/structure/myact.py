6296129accce0f91abbf5fdaca78942e73da3cfd
ZERO-A-ONE <2120220657@mail.nankai.edu.cn>
2023-10-30 23:52:08 +0800

    1

--- sourcecode/structure/myact.py
+++
@@ -1,6 +0,0 @@
-
-class myact:
-    def __init__(self, act):
-        self.act = act
-        self.screenobj = []
-        self.nextact = [] # act -> screen.vector
--- sourcecode/structure/phone.py
+++
@@ -1,8 +0,0 @@
-class usephone:
-    def __init__(self, uiauto, dev_id):
-        """
-        :param uiauto: uiauto的可操作对象
-        :param dev_id: adb的可操作对象名称
-        """
-        self.uiauto = uiauto
-        self.dev_id = dev_id
